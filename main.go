ec4044985394172fa40dfb5e9cdcbaa9a42f4a57
Gustavo Orozco <phersus@hotmail.com>
2021-11-24 21:52:18 +0000

    Providing a consolidated partial result with all the relevant subnet limits from /1 to /32

--- main.go
+++ main.go
@@ -27,29 +27,54 @@ func main() {
 
 	sByte := toInt(sOct) // We get an int version of the IPv4
 
+	// from /32 to /25
 	n4 := checkOdd(sByte[3]) // We check the last octet for oddity
 	_r4 := netBorders(n4)    // We get all the class C subnets 4th octet
-	e := []int{sByte[3]}     //
-	r4 := append(e, _r4...)
+	e := []int{sByte[3]}     // we add sByte[3] to a slice to be able to append to it
+	r4 := append(e, _r4...)  // We append the rest of the netBorders to e
 	fmt.Println(r4)
 
+	// from /24 to /17
 	n3 := checkOdd(sByte[2])
 	_r3 := netBorders(n3)
 	e = []int{sByte[2]}
 	r3 := append(e, _r3...)
 	fmt.Println(r3)
 
+	// from /16 to /9
 	n2 := checkOdd(sByte[1])
 	_r2 := netBorders(n2)
 	e = []int{sByte[1]}
 	r2 := append(e, _r2...)
 	fmt.Println(r2)
 
+	// from /8 to /1
 	n1 := checkOdd(sByte[0])
 	_r1 := netBorders(n1)
 	e = []int{sByte[0]}
 	r1 := append(e, _r1...)
 	fmt.Println(r1)
+
+	n := crtNetBorders(sByte)
+	fmt.Println(n)
+
+}
+
+// crtNetBorders creates all the network subnets from /1 to /32
+func crtNetBorders(bite []int) [][]int {
+	n := make([]int, 4)
+	_r := make([][]int, 4)
+	r := make([][]int, 4)
+	for i := range r {
+		r[i] = make([]int, 8)
+		for j := range n {
+			n[j] = checkOdd(bite[j])
+			_r[j] = netBorders(n[j])
+			r[i] = append([]int{bite[i]}, _r[i]...)
+			// fmt.Println(r[i])
+		}
+	}
+	return r
 }
 
 // checkOdd will receive an integer and returns it if it is even, otherwise it returns the
@@ -71,6 +96,15 @@ func toInt(sOct []string) []int {
 	return sByte
 }
 
+func toStr(sByte []int) []string {
+	sOct := make([]string, 8)
+	for i := range sByte {
+		sOct[i] = strconv.Itoa(sByte[i])
+	}
+	return sOct
+}
+
+// netBorders will provide a []int with the subnet limits on the given octet
 func netBorders(n int) []int {
 	r := make([]int, 7)
 	for i := range v {
