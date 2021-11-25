561d9250ca4f74597b7a9f1bcd53ce7e05805436
Gustavo Orozco <phersus@hotmail.com>
2021-11-25 21:02:30 +0000

    Working feature with annotations and print statements to verify

--- main.go
+++ main.go
@@ -9,49 +9,49 @@ import (
 var (
 	v       = []int{2, 4, 8, 16, 32, 64, 128}
 	netList = []string{
-		"%s.0.0.0./1",
-		"%s.0.0.0./2",
-		"%s.0.0.0./3",
-		"%s.0.0.0./4",
-		"%s.0.0.0./5",
-		"%s.0.0.0./6",
-		"%s.0.0.0./7",
-		"%s.0.0.0./8",
-		"%s.%s.0.0./9",
-		"%s.%s.0.0./10",
-		"%s.%s.0.0./11",
-		"%s.%s.0.0./12",
-		"%s.%s.0.0./13",
-		"%s.%s.0.0./14",
-		"%s.%s.0.0./15",
-		"%s.%s.0.0./16",
-		"%s.%s.%s.0./17",
-		"%s.%s.%s.0./18",
-		"%s.%s.%s.0./19",
-		"%s.%s.%s.0./20",
-		"%s.%s.%s.0./21",
-		"%s.%s.%s.0./22",
-		"%s.%s.%s.0./23",
-		"%s.%s.%s.0./24",
-		"%s.%s.%s.%s/25",
-		"%s.%s.%s.%s/26",
-		"%s.%s.%s.%s/27",
-		"%s.%s.%s.%s/28",
-		"%s.%s.%s.%s/29",
-		"%s.%s.%s.%s/30",
-		"%s.%s.%s.%s/31",
 		"%s.%s.%s.%s/32",
+		"%s.%s.%s.%s/31",
+		"%s.%s.%s.%s/30",
+		"%s.%s.%s.%s/29",
+		"%s.%s.%s.%s/28",
+		"%s.%s.%s.%s/27",
+		"%s.%s.%s.%s/26",
+		"%s.%s.%s.%s/25",
+		"%s.%s.%s.0/24",
+		"%s.%s.%s.0/23",
+		"%s.%s.%s.0/22",
+		"%s.%s.%s.0/21",
+		"%s.%s.%s.0/20",
+		"%s.%s.%s.0/19",
+		"%s.%s.%s.0/18",
+		"%s.%s.%s.0/17",
+		"%s.%s.0.0/16",
+		"%s.%s.0.0/15",
+		"%s.%s.0.0/14",
+		"%s.%s.0.0/13",
+		"%s.%s.0.0/12",
+		"%s.%s.0.0/11",
+		"%s.%s.0.0/10",
+		"%s.%s.0.0/9",
+		"%s.0.0.0/8",
+		"%s.0.0.0/7",
+		"%s.0.0.0/6",
+		"%s.0.0.0/5",
+		"%s.0.0.0/4",
+		"%s.0.0.0/3",
+		"%s.0.0.0/2",
+		"%s.0.0.0/1",
 	}
 )
 
 func main() {
 
-	ip := "172.17.34.79"          // Our initial IPv4 address to analyze
+	ip := "172.17.34.80"          // Our initial IPv4 address to analyze
 	sOct := make([]string, 4)     // We prepare the placeholder for the individual octets
 	sOct = strings.Split(ip, ".") // We separate the octets
 	sByte := toInt(sOct)          // We get an int version of the IPv4
 
-	/* We start computing */
+	/* ####################### We start computing ############################### */
 
 	// from /32 to /25
 	n4 := checkOdd(sByte[3]) // We check the last octet for oddity
@@ -86,92 +86,88 @@ func main() {
 	fmt.Println(n)
 	fmt.Println(nStr)
 
-	fmt.Println(fmt.Sprintf(netList[0], nStr[0][7]))
+	fmt.Println(crtSubNets(sOct, nStr))
 
-	// for i := range nStr {
-	//
-	// 	for j := range nStr[i] {
-	// 		switch i {
-	// 		case 0:
-	// 			switch j {
-	// 			case 0:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/8\n")
-	// 			case 1:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/7\n")
-	// 			case 2:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/6\n")
-	// 			case 3:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/5\n")
-	// 			case 4:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/4\n")
-	// 			case 5:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/3\n")
-	// 			case 6:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/2\n")
-	// 			case 7:
-	// 				fmt.Printf(nStr[i][j] + ".0.0.0/1\n")
-	// 			}
-	// 		case 1:
-	// 			switch j {
-	// 			case 0:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/16\n")
-	// 			case 1:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/15\n")
-	// 			case 2:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/14\n")
-	// 			case 3:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/13\n")
-	// 			case 4:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/12\n")
-	// 			case 5:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/11\n")
-	// 			case 6:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/10\n")
-	// 			case 7:
-	// 				fmt.Printf(sOct[0] + "." + nStr[i][j] + ".0.0/9\n")
-	// 			}
-	// 		case 2:
-	// 			switch j {
-	// 			case 0:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/24\n")
-	// 			case 1:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/23\n")
-	// 			case 2:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/22\n")
-	// 			case 3:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/21\n")
-	// 			case 4:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/20\n")
-	// 			case 5:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/19\n")
-	// 			case 6:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/18\n")
-	// 			case 7:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + nStr[i][j] + ".0/17\n")
-	// 			}
-	// 		case 3:
-	// 			switch j {
-	// 			case 0:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/32\n")
-	// 			case 1:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/31\n")
-	// 			case 2:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/30\n")
-	// 			case 3:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/29\n")
-	// 			case 4:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/28\n")
-	// 			case 5:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/27\n")
-	// 			case 6:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/26\n")
-	// 			case 7:
-	// 				fmt.Printf(sOct[0] + "." + sOct[1] + "." + sOct[2] + "." + nStr[i][j] + "/25\n")
-	// 			}
-	// 		}
-	// 	}
-	// }
+	res := make([]string, 32)
 
+	for i := 0; i <= 7; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], sOct[1], sOct[2], nStr[0][i])
+	}
+
+	for i := 8; i <= 15; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], sOct[1], nStr[1][i-8])
+	}
+
+	for i := 16; i <= 23; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], nStr[2][i-16])
+	}
+
+	for i := 24; i <= 31; i++ {
+		res[i] = fmt.Sprintf(netList[i], nStr[3][i-24])
+	}
+	fmt.Println(res)
+
+	println()
+	println()
+	println()
+	fmt.Println(fmt.Sprintf(netList[0], sOct[0], sOct[1], sOct[2], nStr[0][0]))
+	fmt.Println(fmt.Sprintf(netList[1], sOct[0], sOct[1], sOct[2], nStr[0][1]))
+	fmt.Println(fmt.Sprintf(netList[2], sOct[0], sOct[1], sOct[2], nStr[0][2]))
+	fmt.Println(fmt.Sprintf(netList[3], sOct[0], sOct[1], sOct[2], nStr[0][3]))
+	fmt.Println(fmt.Sprintf(netList[4], sOct[0], sOct[1], sOct[2], nStr[0][4]))
+	fmt.Println(fmt.Sprintf(netList[5], sOct[0], sOct[1], sOct[2], nStr[0][5]))
+	fmt.Println(fmt.Sprintf(netList[6], sOct[0], sOct[1], sOct[2], nStr[0][6]))
+	fmt.Println(fmt.Sprintf(netList[7], sOct[0], sOct[1], sOct[2], nStr[0][7]))
+
+	fmt.Println(fmt.Sprintf(netList[8], sOct[0], sOct[1], nStr[1][0]))
+	fmt.Println(fmt.Sprintf(netList[9], sOct[0], sOct[1], nStr[1][1]))
+	fmt.Println(fmt.Sprintf(netList[10], sOct[0], sOct[1], nStr[1][2]))
+	fmt.Println(fmt.Sprintf(netList[11], sOct[0], sOct[1], nStr[1][3]))
+	fmt.Println(fmt.Sprintf(netList[12], sOct[0], sOct[1], nStr[1][4]))
+	fmt.Println(fmt.Sprintf(netList[13], sOct[0], sOct[1], nStr[1][5]))
+	fmt.Println(fmt.Sprintf(netList[14], sOct[0], sOct[1], nStr[1][6]))
+	fmt.Println(fmt.Sprintf(netList[15], sOct[0], sOct[1], nStr[1][7]))
+
+	fmt.Println(fmt.Sprintf(netList[16], sOct[0], nStr[2][0]))
+	fmt.Println(fmt.Sprintf(netList[17], sOct[0], nStr[2][1]))
+	fmt.Println(fmt.Sprintf(netList[18], sOct[0], nStr[2][2]))
+	fmt.Println(fmt.Sprintf(netList[19], sOct[0], nStr[2][3]))
+	fmt.Println(fmt.Sprintf(netList[20], sOct[0], nStr[2][4]))
+	fmt.Println(fmt.Sprintf(netList[21], sOct[0], nStr[2][5]))
+	fmt.Println(fmt.Sprintf(netList[22], sOct[0], nStr[2][6]))
+	fmt.Println(fmt.Sprintf(netList[23], sOct[0], nStr[2][7]))
+
+	fmt.Println(fmt.Sprintf(netList[24], nStr[3][0]))
+	fmt.Println(fmt.Sprintf(netList[25], nStr[3][1]))
+	fmt.Println(fmt.Sprintf(netList[26], nStr[3][2]))
+	fmt.Println(fmt.Sprintf(netList[27], nStr[3][3]))
+	fmt.Println(fmt.Sprintf(netList[28], nStr[3][4]))
+	fmt.Println(fmt.Sprintf(netList[29], nStr[3][5]))
+	fmt.Println(fmt.Sprintf(netList[30], nStr[3][6]))
+	fmt.Println(fmt.Sprintf(netList[31], nStr[3][7]))
+
+}
+
+func crtSubNets(sOct []string, nStr [][]string) []string {
+
+	res := make([]string, 32)
+
+	for i := 0; i <= 7; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], sOct[1], sOct[2], nStr[0][i])
+	}
+
+	for i := 8; i <= 15; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], sOct[1], nStr[1][i-8])
+	}
+
+	for i := 16; i <= 23; i++ {
+		res[i] = fmt.Sprintf(netList[i], sOct[0], nStr[2][i-16])
+	}
+
+	for i := 24; i <= 31; i++ {
+		res[i] = fmt.Sprintf(netList[i], nStr[3][i-24])
+	}
+	return res
 }
 
 // crtNetBorders creates all the network subnets from /1 to /32
@@ -186,20 +182,14 @@ func crtNetBorders(bite []int) ([][]int, [][]string) {
 	biteStr = toStr(bite)
 
 	for i := range r {
-		// for i := len(r) - 1; i >= 0; i-- {
 		r[i] = make([]int, 8)
 		rStr[i] = make([]string, 8)
-
 		for j := range n {
-			// for j := len(n) - 1; j >= 0; j-- {
 			n[j] = checkOdd(bite[j])
 			_r[j] = netBorders(n[j])
 			_rStr[j] = toStr(_r[j])
 			r[i] = append([]int{bite[3-i]}, _r[3-i]...)
-			// r[i] = append([]int{bite[i]}, _r[i]...)
 			rStr[i] = append([]string{biteStr[3-i]}, _rStr[3-i]...)
-			// rStr[i] = append([]string{biteStr[i]}, _rStr[i]...)
-			// fmt.Println(r[i])
 		}
 	}
 	return r, rStr
